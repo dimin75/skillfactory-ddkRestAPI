a4dc855207e4bf9faf18b81e98802fa1f94e50e3
dimin75 <dmitry.kaplunenko@gmail.com>
2024-04-13 21:15:03 +1000

    Add Swagger UI documentation to README.md

--- ddkDbRestAPI/settings.py
+++ ddkDbRestAPI/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import drf_yasg
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'dbpereval'
 ]
 
--- ddkDbRestAPI/urls.py
+++ ddkDbRestAPI/urls.py
@@ -18,11 +18,28 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Документация по функциям RestAPI",
+      default_version='v1',
+      description="Описание API-функций",
+      terms_of_service="https://www.example.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('', include('dbpereval.urls')),
-   path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
