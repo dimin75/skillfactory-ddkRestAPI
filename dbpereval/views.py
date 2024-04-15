59d08b3200bd379abc39563be3ad60e639494ad3
dimin75 <dmitry.kaplunenko@gmail.com>
2024-04-15 21:37:41 +1000

    Add yaml2md converter and its working result 2nd ver

--- dbpereval/views.py
+++ dbpereval/views.py
@@ -27,6 +27,42 @@ from rest_framework.renderers import DocumentationRenderer
 #     patterns=None,
 # )
 
+def yaml_to_markdown(yaml_str):
+    data = yaml.safe_load(yaml_str)
+    markdown = f"# {data['info']['title']}\n\n"
+    markdown += f"## {data['info']['description']}\n\n"
+    markdown += f"* Версия: {data['info']['version']}\n"
+    markdown += f"* Условия использования: [{data['info']['termsOfService']}]({data['info']['termsOfService']})\n"
+    markdown += f"* Контакт: {data['info']['contact']['email']}\n"
+    markdown += f"* Лицензия: {data['info']['license']['name']}\n\n"
+    markdown += "### Базовая информация\n\n"
+    markdown += f"* Хост: {data['host']}\n"
+    markdown += f"* Протоколы: {', '.join(data['schemes'])}\n"
+    markdown += f"* Базовый путь: {data['basePath']}\n\n"
+    markdown += "### Форматы данных\n\n"
+    markdown += f"* Входные данные: {', '.join(data['consumes'])}\n"
+    markdown += f"* Выходные данные: {', '.join(data['produces'])}\n\n"
+    markdown += "### Безопасность\n\n"
+    markdown += f"* Описание: {', '.join(data['securityDefinitions'])}\n\n"
+    markdown += "## Маршруты\n\n"
+    for path, path_data in data['paths'].items():
+        markdown += f"### {path}\n\n"
+        for method, method_data in path_data.items():
+            markdown += f"#### {method.upper()} {path}\n\n"
+            if isinstance(method_data, list):
+                continue  # Пропускаем список, так как не можем получить описание
+            markdown += f"* Описание: {method_data.get('description', '')}\n"
+            if 'parameters' in method_data:
+                markdown += "* Параметры:\n"
+                for param in method_data['parameters']:
+                    markdown += f"  - {param['name']}: {'обязательный' if param.get('required', False) else 'необязательный'}, {param['type']}, в {param['in']}\n"
+            if 'responses' in method_data:
+                markdown += "* Ответ:\n"
+                for resp_code, resp_data in method_data['responses'].items():
+                    markdown += f"  - {resp_code}: {resp_data['description']}\n"
+            markdown += "\n"
+    return markdown
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Документация по функциям RestAPI",
@@ -57,7 +93,9 @@ def generate_markdown(request):
 
     generator = schema_view.with_ui('swagger')
     schema = generator.get_schema(request=request)
-    markdown_data = schema.to_markdown()
+    yaml_data = schema.yaml()
+    markdown_data = yaml_to_markdown(yaml_data)
+    # markdown_data = schema.to_markdown()
 
     # Сохраняем Markdown-файл на диск
     with open('swagger.md', 'w') as f:
